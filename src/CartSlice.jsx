c4a08779e4b7b589444de31f21297d9248d7f107
shailveekishore-cell <shailveekishore@gmail.com>
2026-06-06 12:11:14 -0400

    Fix formatting in CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -20,4 +20,6 @@ export const CartSlice = createSlice({
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
-export default CartSlice.reducer;
+export default CartSlice.reducer; 
+
+
